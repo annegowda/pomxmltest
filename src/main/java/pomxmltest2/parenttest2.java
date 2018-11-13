041d9cf8de4b6c13bbf55fad48e4137b9eb638bd
Annegowda C N <agowda967@gmail.com>
2018-11-13 15:44:24 +0530

    intial commit

---
+++ src/main/java/pomxmltest2/parenttest2.java
@@ -0,0 +1,18 @@
+package pomxmltest2;
+
+import org.testng.annotations.Test;
+
+public class parenttest2 {
+	
+	@Test
+	public void m11() {
+		System.out.println("im test-3");
+	}
+
+	
+	@Test
+	public void m12() {
+		System.out.println("im test-4");
+	}
+
+}
---
+++ src/test/java/package1/Parenttest.java
@@ -0,0 +1,19 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class Parenttest {
+	
+	@Test
+	public void m11() {
+		System.out.println("im test-1");
+	}
+
+	
+	@Test
+	public void m12() {
+		System.out.println("im test-2");
+	}
+
+
+}
